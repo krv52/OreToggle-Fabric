699348c13fd02c4a2ff03e80316be67c2f71c1fa
krv52 <alexusach123@gmail.com>
2026-05-01 02:15:07 +0200

    Initial Fabric mod setup

---
+++ src/main/java/me/krv/oretoggle/OreToggleFabric.java
@@ -0,0 +1,10 @@
+package me.krv.oretoggle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OreToggleFabric implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("OreToggle Fabric loaded");
+	}
+}
